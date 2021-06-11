0e405cdb6429fc9c23f23c58864eabb5d8b362c1
Gwendal <gwendou95@hotmail.fr>
2021-06-11 17:37:42 +0200

    Initial commit

---
+++ Whats-up-doc/Whats_up_docApp.swift
@@ -0,0 +1,28 @@
+//
+//  Whats_up_docApp.swift
+//  Whats-up-doc
+//
+//  Created by Etudiant on 11/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct Whats_up_docApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TabView {
+                Color(.blue)
+                    .tabItem {
+                        Text("Home")
+                        Image(systemName: "house.fill")
+                    }
+                Color(.purple)
+                    .tabItem {
+                        Text("Diagnostics")
+                        Image(systemName: "waveform.path.ecg")
+                    }
+            }
+        }
+    }
+}
